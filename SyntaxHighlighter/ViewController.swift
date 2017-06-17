e54e81512141e92bfbb258a9a95270ad9dc40da6
Joel Cummings <joelcummings@mac.com>
2017-06-16 23:38:01 -0400

    Initial Commit

---
+++ SyntaxHighlighter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SyntaxHighlighter
+//
+//  Created by Joel Cummings on 2017-06-16.
+//  Copyright © 2017 Joel Cummings. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
